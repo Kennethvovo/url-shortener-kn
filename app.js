4476a2684705ffe0c469830ff6579a061cf42b04
Kennethvovo <vovo811111@gmail.com>
2021-11-21 21:23:44 +0800

    feat:initialize project

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const { engine } = require('express-handlebars')
+const PORT = 3000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/url-shortener')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.listen(PORT, () => {
+  console.log(`The server is running on http://localhost:${PORT}!`)
+})
