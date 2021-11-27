d97974ba2b0c5aab5748f0bab325a820306a2881
Kennethvovo <vovo811111@gmail.com>
2021-11-27 18:07:25 +0800

    remove body-parser

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { engine } = require('express-handlebars')
-const bodyParser = require('body-Parser')
 require('./config/mongoose')
 const app = express()
 const routes = require('./routes')
 const PORT = process.env.port || 3000
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(routes)
 
