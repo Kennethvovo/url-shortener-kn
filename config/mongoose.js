25425453d83d18b260d77d978dd5b2cbd3b2dc8f
Kennethvovo <vovo811111@gmail.com>
2021-11-27 18:05:23 +0800

    feat :fix moogoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/url-list'
 const db = mongoose.connection
-
+mongoose.connect(MONGODB_URI)
 db.on('error', () => {
   console.log('mongodb error!')
 })
